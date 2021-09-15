5116ead16fe46799f63404cae63208382992a833
Semih Bali <66493998+SemihBL@users.noreply.github.com>
2021-09-15 09:25:12 +0300

    FileUpdating

--- NodeJS/controllers/errors.js
+++
@@ -1,5 +0,0 @@
-module.exports.getError = (req, res, next) => {
-    res.status(404);
-    res.render('./error/404', { title: "Page not found" })
-    // res.sendFile(path.join(__dirname, 'views', '404.html'));
-}
